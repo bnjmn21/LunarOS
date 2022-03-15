4c45eadac5702deb30d28f686f181a94597c6dce
bnjmn21 <68250874+bnjmn21@users.noreply.github.com>
2022-03-15 20:03:18 +0100

    ok

---
+++ install/LuaOS.lua
@@ -0,0 +1,16 @@
+local function getRepoFile(path)
+    
+end
+
+term.clear()
+term.setCursorPos(1, 1)
+term.write("\n\n\n")
+term.write("===LUAOS-INSTALLER===\n")
+term.write("This Software will install the LuaOS\nonto this PC. Additional Packages will be installed.")
+term.write("===\n")
+term.write("Continuing in 5 seconds...\n")
+os.sleep(5)
+term.setBackgroundColor(colors.blue)
+term.clear()
+term.setCursorPos(1, 1)
+term.write("Installation Phase 1:\n")
