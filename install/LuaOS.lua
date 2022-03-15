ea032df120a7ace05b957c521e89fe16721a8bcd
bnjmn21 <68250874+bnjmn21@users.noreply.github.com>
2022-03-15 20:28:11 +0100

    save

--- install/LuaOS.lua
+++ install/LuaOS.lua
@@ -1,5 +1,25 @@
+local function checkNetwork()
+    if not http then
+        term.setTextColor(colors.red)
+        term.write("You must activate the http module in\nthe computercraft server config file to\ninstall LuaOS.\n")
+        return true
+    end
+    if not http.checkURL("https://githubusercontent.com") then
+        term.setTextColor(colors.red)
+        term.write("Could not verify github.com(githubusercontent.com).\nMaybe the website was disabled by the server owner.\nGithub is required to install necessary Software.\n")
+        return true
+    end
+    if not http.get("https://raw.githubusercontent.com/bnjmn21/LuaOS/main/README.md") then
+        term.setTextColor(colors.red)
+        term.write("Failed to connect to github. Check your internet connection.\n")
+        return true
+    end
+end
 local function getRepoFile(path)
-    
+    local res = http.get("https://raw.githubusercontent.com/bnjmn21/LuaOS/main/"+path)
+    local str = res.readAll()
+    res.close()
+    return str
 end
 
 term.clear()
@@ -10,7 +30,12 @@ term.write("This Software will install the LuaOS\nonto this PC. Additional Packa
 term.write("===\n")
 term.write("Continuing in 5 seconds...\n")
 os.sleep(5)
-term.setBackgroundColor(colors.blue)
 term.clear()
 term.setCursorPos(1, 1)
 term.write("Installation Phase 1:\n")
+local canConnect = checkNetwork()
+if canConnect then
+    exit()
+end
+term.setTextColor(colors.lime)
+term.write("Network check completed!\n")
