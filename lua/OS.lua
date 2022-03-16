d97a2b1087d6dd9c72e622fb778fff03def404e0
bnjmn21 <68250874+bnjmn21@users.noreply.github.com>
2022-03-16 22:11:17 +0100

    update

--- lua/OS.lua
+++ lua/OS.lua
@@ -1,4 +1,21 @@
 OS = {}
+local btns = {}
+
+local SETTINGS = {
+    ["FunctionBar"] = {
+        ["DisplayFunctionBar"] = true
+    }
+}
+
+local function init()
+    if SETTINGS["FunctionBar"]["DisplayFunctionBar"] then
+        homebtn = {
+        }
+
+        table.insert(btns)
+    end
+end
+
 
 local function draw()
     
